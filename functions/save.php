82280ef0d2be9959c3fae99b227686a93bbd065d
Elijah <elijahbee@ovi.com>
2014-11-07 06:20:36 +0300

    Save records for later

--- functions/save.php
+++ functions/save.php
@@ -1,23 +1,21 @@
 <?php
-include 'connect.php';
+include_once 'connect.php';
 
 // Gets data from URL parameters
-$name = $_GET['name'];
-$address =  mysql_real_escape_string($_GET['address']);
-$lat =  mysql_real_escape_string($_GET['lat']);
-$lng =  mysql_real_escape_string($_GET['lng']);
-$type =  mysql_real_escape_string($_GET['type']);
-$token =  mysql_real_escape_string($_GET['token']);
+$name = mysql_real_escape_string(trim($_GET['name']));
+$address =  mysql_real_escape_string(trim($_GET['address']));
+$lat =  mysql_real_escape_string(trim($_GET['lat']));
+$lng =  mysql_real_escape_string(trim($_GET['lng']));
+$type =  mysql_real_escape_string(trim($_GET['type']));
+$token =  mysql_real_escape_string(trim($_GET['token']));
 
 // Check if token already exists
-$count = "Select count(*) as count from markers where token = '$token'";
+$count = "Select * from markers where token = '$token'";
 $i = mysql_query($query);
+$count = mysql_num_rows($i);
 
-echo $i;
-die();
-
-if($i == 0){
-// Insert new row with user data
+if($count < 1){
+// Insert new record with token information
 $query = sprintf("INSERT INTO markers " .
          " (name, address, lat, lng, token, type ) " .
          " VALUES ('%s', '%s', '%s', '%s', '%s', '%s');",
@@ -30,6 +28,8 @@ $query = sprintf("INSERT INTO markers " .
 		}else{
 			echo 1;
 		}
-		
+}else{
+	// Fetch additional information, doctors registered here etc
+	echo '';
 }
 ?>
